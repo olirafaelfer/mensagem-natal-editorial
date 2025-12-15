a66130e0f1dfd9c60e933416b1619608ac7cc6fc
olirafaelfer <oli.rafael.fer@gmail.com>
2025-12-15 06:59:27 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,11 +6,15 @@ import { getFirestore, doc, getDoc, runTransaction, serverTimestamp } from
   "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
 const firebaseConfig = {
-  // apiKey: "...",
-  // authDomain: "...",
-  // projectId: "...",
-  // appId: "..."
+  apiKey: "AIzaSyD_-M7m1R2-FKzOHg356vb_IN7bPb6hqJM",
+  authDomain: "missao-natal-ranking.firebaseapp.com",
+  projectId: "missao-natal-ranking",
+  storageBucket: "missao-natal-ranking.firebasestorage.app",
+  messagingSenderId: "175157868358",
+  appId: "1:175157868358:web:690742496c05983d1c1747",
+  measurementId: "G-24ZVFPGSYQ"
 };
+
 
 let db = null;
 function hasFirebaseConfig(cfg){ return !!(cfg && cfg.apiKey && cfg.projectId && cfg.appId); }
